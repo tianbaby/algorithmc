d374ef880635b4dc9b2011c7d12f6a0dad7066c1
tianbaby <2445942666@qq.com>
2015-11-15 18:43:24 +0800

    第一个算法桶排序

---
+++ 1.桶排序/桶排序/main.c
@@ -0,0 +1,26 @@
+//
+//  main.c
+//  桶排序
+//
+//  Created by 凉鞋 on 15/11/14.
+//  Copyright © 2015年 凉鞋. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    int a[11],i,j,k;
+    for(i=0;i<=10;i++)
+        a[i]=0;         // 初始化为0
+    
+    
+    for(i=0;i<5;i++)
+    {
+        scanf("%d",&j); //把每一个数读到变量j中
+        a[j]++;         //进行计数
+    }
+    
+    for(i=0;i<10;i++)
+        for(k=1;k<=a[i];)
+    return 0;
+}
